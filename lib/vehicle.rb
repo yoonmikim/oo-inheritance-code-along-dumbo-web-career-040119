c52e031e40281c34e91c87635ff4e73f98c9e47a
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-04 20:21:55 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,7 +1,7 @@
 class Vehicle 
   attr_accessor :wheel_size, :wheel_number
   
-  def initialize(wheel_size, wheel_size)
+  def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
